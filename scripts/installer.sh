1110430256696ebdc8888e4474174c457ba826a3
Eric Chu <chu.eric@gmail.com>
2020-05-28 09:41:14 +0700

    Pull ab-cli from installer script

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -v 
 
-#run installer
 WORKING_DIR=workdir
 
+#update installer
+time \
+npm install -g Hiro-Nakamura/ab-cli
+
+#run installer
 time \
 appbuilder install $WORKING_DIR \
 	--V1 \
@@ -21,5 +25,6 @@ appbuilder install $WORKING_DIR \
 	--smtp.enable=false \
 	--smtp.auth=plain
 
+#link in test configuration for docker swarm
 ln -s ../docker-compose.test.yml workdir/docker-compose.test.yml
 
